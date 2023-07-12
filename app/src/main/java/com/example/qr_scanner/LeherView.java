2238b4913fcacc66427532116f50179ef265958c
Kunzmann Daniel <dani.k1806@gmail.com>
2023-07-12 19:04:33 +0200

    Änderungen im LehrerView

--- app/src/main/java/com/example/qr_scanner/LeherView.java
+++ app/src/main/java/com/example/qr_scanner/LeherView.java
@@ -1,8 +1,8 @@
 package com.example.qr_scanner;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LeherView extends AppCompatActivity {
 
@@ -10,5 +10,38 @@ public class LeherView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leher_view);
+
+        Button buttonAusgabe = findViewById(R.id.buttonAusgabe);
+        Button buttonAnnahme = findViewById(R.id.buttonAnnahme);
+        Button buttonNeu = findViewById(R.id.buttonNeuesGeraetBuch);
+        Button buttonLoeschen = findViewById(R.id.buttonGeraetBuchLoeschen);
+
+        buttonAusgabe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Hier folgt die Ausgabe (Datenbank usw.)
+            }
+        });
+
+        buttonAnnahme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Hier folgt die Annahme (Datenbank usw.)
+            }
+        });
+
+        buttonNeu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Hier folgt die Eintragung von etwas neuem (Datenbank usw.)
+            }
+        });
+
+        buttonLoeschen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // //Hier folgt die Austragung von etwas (Datenbank usw.)
+            }
+        });
     }
 }
