33805e9a442e4e09748b756b187260a5e2c40bde
zWaschweib <haiderl@bswiesaude.onmicrosoft.com>
2023-07-12 12:45:27 +0200

    Scanner

--- app/src/main/java/com/example/qr_scanner/login.java
+++ app/src/main/java/com/example/qr_scanner/login.java
@@ -3,12 +3,20 @@ package com.example.qr_scanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
+
+TextView txtBname, txtPwd;
+Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
     }
 }
