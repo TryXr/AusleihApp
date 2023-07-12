b6385cb470577db824658e3552a61d4d51a599c5
zWaschweib <haiderl@bswiesaude.onmicrosoft.com>
2023-07-12 12:49:18 +0200

    Intent

--- app/src/main/java/com/example/qr_scanner/login.java
+++ app/src/main/java/com/example/qr_scanner/login.java
@@ -2,12 +2,14 @@ package com.example.qr_scanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class login extends AppCompatActivity {
+public class login extends AppCompatActivity implements View.OnClickListener {
 
 TextView txtBname, txtPwd;
 Button btnSubmit;
@@ -17,6 +19,17 @@ Button btnSubmit;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        txtBname = findViewById(R.id.editTextUsername);
+        txtPwd = findViewById(R.id.editTextPassword);
+        btnSubmit = findViewById(R.id.buttonLogin);
 
+        btnSubmit.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
