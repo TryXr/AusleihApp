2b23b8b59c9afa7034c70a7ea41f0331f73c3b25
TryXr <dani.k1806@gmail.com>
2023-07-13 11:49:16 +0200

    AusgabeView UI

---
+++ app/src/main/java/com/example/qr_scanner/AusgabeView.java
@@ -0,0 +1,15 @@
+package com.example.qr_scanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AusgabeView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ausgabe_view);
+
+    }
+}
