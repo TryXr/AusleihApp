53e77ddaf0999c4d99fbf1ad9a73c393453bf643
TryXr <dani.k1806@gmail.com>
2023-07-18 11:59:06 +0200

    Datanbank zeugs für Schüler

---
+++ app/src/main/java/com/example/qr_scanner/NewObjectView.java
@@ -0,0 +1,14 @@
+package com.example.qr_scanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewObjectView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_object_view);
+    }
+}
